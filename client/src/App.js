611c54b445560a0d76cda9169e8416f29eb06a16
Kamilah Bass <kamzbass@gmail.com>
2022-12-05 04:22:45 -0400

    form

--- client/src/App.js
+++ client/src/App.js
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import React from 'react';
 import './App.css';
 
@@ -14,11 +15,32 @@ function App() {
       <section id="section2">
         <div class="formArea">
           <main>
-            <form action="/#">
-              <label for="fname">First Name:</label><br></br>
-              <input type="text" id="fname" name="fname" value="First Name" /><br></br>
+           <form method="get" action="#" id="login-form" class="login-form" autocomplete="off" role="main">
+             <h1 class="a11y-hidden">Login Form</h1>
+              <div>
+                <label class="label-email">
+                 <input type="email" class="text" name="email" placeholder="Email" tabindex="1" required />
+                 <span class="required">Email</span>
+                </label>
+              </div>
+             <input type="checkbox" name="show-password" class="show-password a11y-hidden" id="show-password" tabindex="3" />
+             <label class="label-show-password" for="show-password">
+                <span>Show Password</span>
+             </label>
+             <div>
+                <label class="label-password">
+                 <input type="text" class="text" name="password" placeholder="Password" tabindex="2" required />
+                    <span class="required">Password</span>
+               </label>
+              </div>
+             <input type="submit" value="Log In" />
+              <div class="email">
+               
+               <a href="#">Forgot password?</a>
+             </div>
+             
             </form>
-          </main>
+         </main>
         </div>
       </section> 
     </div>
