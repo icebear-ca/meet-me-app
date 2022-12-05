c5fa4d7282f9271aab590b47a9da11cdfdfc605d
Kamilah Bass <kamzbass@gmail.com>
2022-12-05 14:01:01 -0400

    form2

--- client/src/scenes/loginPage/Form.jsx
+++ client/src/scenes/loginPage/Form.jsx
@@ -32,3 +32,158 @@ const loginSchema = yup.object().shape({
     password: yup.string().required("required"),
 });
 
+const initialValuesRegister = {
+    firstName: "",
+    lastName: "",
+    email: "",
+    password: "",
+    location: "",
+    ocupation: "",
+    picture: "",
+};
+
+const initialValuesLogin = {
+    email: "",
+    password: "",
+};
+
+const Form = () => {
+    const [pageType, setPageType] = useState("login");
+    const { palette } = useTheme();
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+    const isNonMobile = useMediaQuery("(min-width:600px)");
+    const isLogin = pageType === "login";
+    const isRegister = pageType === "register";
+
+    const register = async (values, onSubmitProps) => {
+        const formData = new FormData();
+        for (let value in values) {
+            formData.append(value, values[value]);
+        }
+        formData.append("picturePath", values.picture.name);
+
+        const savedUserResponse = await fetch(
+            "http://localhost:3001/auth/register",
+            {
+                method: "POST",
+                body: formData,
+            }
+        );
+        const savedUser = await savedUserResponse.json();
+        onSubmitProps.resetForm();
+
+        if (savedUser) {
+            setPageType("login");
+        }
+    };
+
+    const login = async (values, onSubmitProps) => {
+        const loggedInResponse = await fetch("http://localhost:3001/auth/login", {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify(values),
+        }); 
+        const loggedIn = await loggedInResponse.json();
+        onSubmitProps.resetForm();
+        if (loggedIn) {
+            dispatch(
+                setLogin({
+                    user: loggedIn.user,
+                    token: loggedIn.token,
+                })
+            );
+            navigate("/home");
+        }
+    };
+
+    const handleFormSubmit = async (values, onSubmitProps) => {
+        if (isLogin) await login(values, onSubmitProps);
+        if (isRegister) await register(values, onSubmitProps);
+    };
+
+    return (
+        <Formik
+            onSubmit={handleFormSubmit}
+            initialValues={isLogin ? initialValuesLogin : initialValuesRegister}
+            validationSchema={isLogin ? loginSchema : registerSchema}
+            >
+                {({
+                    values,
+                    errors,
+                    touched,
+                    handleBlur,
+                    handleChange,
+                    handleSubmit,
+                    setFieldValue,
+                    resetForm,
+                }) => (
+                    <form onSubmit={handleSubmit}>
+                        <Box 
+                            display="grid"
+                            gap="30px"
+                            gridTemplateColumns="repeat(4, minmax(0, 1fr))"
+                            sx={{
+                                "& > div": { gridColumn: isNonMobile ? undefined: "span 4" },
+                            }}
+                            >
+                                {isRegister && (
+                                    <>
+                                        <TextField
+                                            label="First Name"
+                                            onBlur={handleBlur}
+                                            onChange={handleChange}
+                                            value={values.firstName}
+                                            name="firstName"
+                                            error={
+                                                Boolean(touched.firstName) && Boolean(errors.firstName)
+                                            }
+                                            helperText={touched.firstName && errors.firstName}
+                                            sx={{ gridColumn: "span 2" }}
+                                        />
+                                        <TextField 
+                                            label="Last Name"
+                                            onBlur={handleBlur}
+                                            onChange={handleChange}
+                                            value={values.lastName}
+                                            name="firstName"
+                                            error={Boolean(touched.lastName) && Boolean(errors.lastName)}
+                                            helperText={touched.lastName && errors.lastName}
+                                            sx={{ gridColumn: "span 2" }}
+                                        />
+                                        <TextField
+                                            label="Location"
+                                            onBlur={handleBlur}
+                                            onChange={handleChange}
+                                            value={values.location}
+                                            name="location"
+                                            error={Boolean(touched.location) && Boolean(errors.location)}
+                                            helperText={touched.location && errors.location}
+                                            sx={{ gridColumn: "span 4" }}
+                                        />
+                                        <TextField
+                                            label="Occupation"
+                                            onBlur={handleBlur}
+                                            onChange={handleChange}
+                                            value={values.occupation}
+                                            name="occupation"
+                                            error={Boolean(touched.occupation) && Boolean(errors.occupation)}
+                                            helperText={touched.occupation && errors.occupation}
+                                            sx={{ gridColumn: "span 4" }}
+                                            />
+                                        <Box
+                                            gridColumn="span4"
+                                            border={`1px solid ${palette.neutral.medium}`}
+                                            borderRadius="5px"
+                                            p="1rem"
+                                            >
+                                                
+                                            </Box>
+                                     </>
+                                )}
+                            </Box>
+                    </form>
+                )}
+            </Formik>
+    )
+}
